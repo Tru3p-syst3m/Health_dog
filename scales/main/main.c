fd025c9b4687d748ed68e2d9822593e45a28cd7e
Truep <pvorman54@gmail.com>
2026-05-13 17:34:52 +0700

    scales wire update

--- scales/main/main.c
+++ scales/main/main.c
@@ -1,20 +1,16 @@
 #include "driver/gpio.h"
-#include "esp_event.h"
 #include "esp_log.h"
-#include "esp_system.h"
-#include "esp_vfs_dev.h"
-#include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "mqtt_client.h"
+#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-// WiFi setting
-#define WIFI_SSID "BLESSING"
-#define WIFI_PASS "nice cock!"
-static const char *WIFI_TAG = "WIFI";
+// UART setting
+#define UART_PORT UART_NUM_0
+#define UART_BUF_SIZE 64
+#define CMD_BUF_SIZE 32
 
 // HX711 setting
 #define SCALE 384.279083
@@ -30,13 +26,6 @@ typedef struct {
 } hx711_t;
 
 static const char *HX711_TAG = "HX711";
-
-// mqtt setting
-#define MQTT_BROKER_URI "mqtt://192.168.3.2:1883"
-static const char *MQTT_TAG = "MQTT";
-
-int mqtt_connection;
-int input_flag = 0;
 
 void hx711_init(hx711_t *hx, int dt_pin, int sck_pin) {
   hx->dt_pin = dt_pin;
@@ -113,67 +102,6 @@ float hx711_get_units(hx711_t *hx, int times) {
   return (float)value / hx->scale;
 }
 
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
-                               int32_t event_id, void *event_data) {
-  esp_mqtt_event_handle_t event = event_data;
-
-  switch ((esp_mqtt_event_id_t)event_id) {
-  case MQTT_EVENT_CONNECTED:
-    int msg_id = esp_mqtt_client_subscribe(event->client, "weight/in", 1);
-    mqtt_connection = 1;
-    ESP_LOGI(MQTT_TAG, "MQTT подключен! msg_id = %d", msg_id);
-    break;
-
-  case MQTT_EVENT_DISCONNECTED:
-    ESP_LOGI(MQTT_TAG, "MQTT отключен");
-    mqtt_connection = 0;
-    break;
-
-  case MQTT_EVENT_DATA:
-    ESP_LOGI(MQTT_TAG, "Получен сигнал");
-    char message[64];
-    int len = event->data_len < sizeof(message) - 1 ? event->data_len
-                                                    : sizeof(message) - 1;
-    memcpy(message, event->data, len);
-    message[len] = '\0';
-    if (strcmp(message, "get") == 0) {
-      input_flag = 1;
-    }
-    break;
-
-  case MQTT_EVENT_ERROR:
-    ESP_LOGI(MQTT_TAG, "MQTT ошибка");
-    break;
-  default:
-    break;
-  }
-}
-
-void wifi_connect_simple() {
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-  esp_netif_create_default_wifi_sta();
-
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  cfg.nvs_enable = false;
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-  wifi_config_t wifi_config = {
-      .sta =
-          {
-              .ssid = WIFI_SSID,
-              .password = WIFI_PASS,
-              .scan_method = WIFI_ALL_CHANNEL_SCAN,
-              .sort_method = WIFI_CONNECT_AP_BY_SIGNAL,
-          },
-  };
-
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_start());
-  ESP_ERROR_CHECK(esp_wifi_connect());
-}
-
 void app_main() {
   // HX711 init
   hx711_t scales;
@@ -181,31 +109,32 @@ void app_main() {
   hx711_tare(&scales, 10);
   hx711_set_scale(&scales, SCALE);
 
-  // wifi init
-  wifi_connect_simple();
+  // Переводим stdin в неблокирующий режим (работает через VFS консоли)
+  int flags = fcntl(STDIN_FILENO, F_GETFL);
+  fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 
-  vTaskDelay(pdMS_TO_TICKS(10000));
+  printf("Scales ready. Send 'get' to read weight.\n");
 
-  // mqtt init
-  esp_mqtt_client_config_t mqtt_cfg = {
-      .broker.address.uri = MQTT_BROKER_URI,
-  };
-  esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-  esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler,
-                                 NULL);
-  esp_mqtt_client_start(client);
+  char cmd_buf[32] = {0};
+  int buf_len = 0;
 
-  // main cycle
-  char message[64];
   while (1) {
-    if (input_flag == 1) {
-      float weight = hx711_get_units(&scales, 10);
-      sprintf(message, "%.2f", weight);
-      ESP_LOGI(HX711_TAG, "Weight: %s g", message);
-      esp_mqtt_client_publish(client, "weight/out", message, strlen(message), 2,
-                              0);
-      input_flag = 0;
+    int c = fgetc(stdin);
+    if (c != EOF) {
+      if (c == '\n' || c == '\r') {
+        cmd_buf[buf_len] = '\0';
+        if (strcmp(cmd_buf, "get") == 0) {
+          float weight = hx711_get_units(&scales, 10);
+          printf("%.2f\n", weight);
+          fflush(stdout); // Мгновенная отправка в порт
+        }
+        buf_len = 0;
+        memset(cmd_buf, 0, sizeof(cmd_buf));
+      } else if (buf_len < sizeof(cmd_buf) - 1) {
+        cmd_buf[buf_len++] = (char)c;
+      }
     }
-    vTaskDelay(pdMS_TO_TICKS(500));
+    // Проверка буфера ровно раз в секунду, как ты просил
+    vTaskDelay(pdMS_TO_TICKS(20));
   }
 }
